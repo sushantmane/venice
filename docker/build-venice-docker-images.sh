1bea0cd91eb341e9bced96b831d691a9e7292485
Sushant Mane <sumane@linkedin.com>
2022-10-24 00:06:08 -0700

    Fix dockerfiles

---
+++ docker/build-venice-docker-images.sh
@@ -0,0 +1,7 @@
+
+cp ../clients/venice-push-job/build/libs/venice-push-job-all.jar        venice-client/
+cp ../clients/venice-thin-client/build/libs/venice-thin-client-all.jar  venice-client/
+cp ../clients/venice-admin-tool/build/libs/venice-admin-tool-all.jar    venice-client/
+cp ../services/venice-server/build/libs/venice-server-all.jar           venice-server/
+cp ../services/venice-controller/build/libs/venice-controller-all.jar   venice-controller/
+cp ../services/venice-router/build/libs/venice-router-all.jar           venice-router/
