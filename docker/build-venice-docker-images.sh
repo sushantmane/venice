29f5b320dfefd3c2169d9bef4173e07957612f1e
Sushant Mane <sumane@linkedin.com>
2022-10-24 01:57:45 -0700

    fix build file

--- docker/build-venice-docker-images.sh
+++ docker/build-venice-docker-images.sh
@@ -1,15 +1,19 @@
+cd ../
+./gradlew shadowJar
+cd docker
+
 head_hash=$(git rev-parse --short HEAD)
 
-cp ../clients/venice-push-job/build/libs/venice-push-job-all.jar        venice-client/
-cp ../clients/venice-thin-client/build/libs/venice-thin-client-all.jar  venice-client/
-cp ../clients/venice-admin-tool/build/libs/venice-admin-tool-all.jar    venice-client/
-cp ../services/venice-server/build/libs/venice-server-all.jar           venice-server/
-cp ../services/venice-controller/build/libs/venice-controller-all.jar   venice-controller/
-cp ../services/venice-router/build/libs/venice-router-all.jar           venice-router/
+cp ../clients/venice-push-job/build/libs/venice-push-job-all.jar venice-client/
+cp ../clients/venice-thin-client/build/libs/venice-thin-client-all.jar venice-client/
+cp ../clients/venice-admin-tool/build/libs/venice-admin-tool-all.jar venice-client/
+cp ../services/venice-server/build/libs/venice-server-all.jar venice-server/
+cp ../services/venice-controller/build/libs/venice-controller-all.jar venice-controller/
+cp ../services/venice-router/build/libs/venice-router-all.jar venice-router/
 
 targets=(venice-controller venice-server venice-router venice-client)
 
-for target in ${targets}; do
+for target in ${targets[@]}; do
     docker build -t "$target:$head_hash" $target
 done
 
