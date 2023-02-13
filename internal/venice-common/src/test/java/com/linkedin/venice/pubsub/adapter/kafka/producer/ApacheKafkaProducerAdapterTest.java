8b58265fbfdbe584c614d3edc82d8026efde6155
Sushant Mane <sumane@linkedin.com>
2023-02-13 03:23:57 -0800

    Add test

--- internal/venice-common/src/test/java/com/linkedin/venice/pubsub/adapter/kafka/producer/ApacheKafkaProducerAdapterTest.java
+++ internal/venice-common/src/test/java/com/linkedin/venice/pubsub/adapter/kafka/producer/ApacheKafkaProducerAdapterTest.java
@@ -1,11 +1,13 @@
 package com.linkedin.venice.pubsub.adapter.kafka.producer;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 
 import com.linkedin.venice.exceptions.VeniceException;
 import com.linkedin.venice.kafka.protocol.KafkaMessageEnvelope;
 import com.linkedin.venice.message.KafkaKey;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.mockito.Mockito;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -15,14 +17,16 @@ public class ApacheKafkaProducerAdapterTest {
   private ApacheKafkaProducerConfig mockProducerConfig;
 
   @BeforeMethod
-  public void setup() {
+  public void setupMocks() {
     mockKafkaProducer = mock(KafkaProducer.class);
     mockProducerConfig = mock(ApacheKafkaProducerConfig.class);
   }
 
-  @Test(expectedExceptions = VeniceException.class)
+  @Test(expectedExceptions = VeniceException.class, expectedExceptionsMessageRegExp = "The internal KafkaProducer has been closed")
   public void testEnsureProducerIsNotClosedThrowsExceptionWhenProducerIsClosed() {
     ApacheKafkaProducerAdapter producerAdapter = new ApacheKafkaProducerAdapter(mockProducerConfig, mockKafkaProducer);
-    // when(mockKafkaProducer).
+    Mockito.doNothing().when(mockKafkaProducer).close(any());
+    producerAdapter.close(10, false);
+    producerAdapter.getNumberOfPartitions("x");
   }
 }
