92f61d5294cc05931b865738ee0cf4dfaf32c7ea
Sushant Mane <sumane@linkedin.com>
2023-02-14 02:25:57 -0800

    Add UTs for kafka conig class

--- internal/venice-common/src/test/java/com/linkedin/venice/pubsub/adapter/kafka/producer/ApacheKafkaProducerConfigTest.java
+++ internal/venice-common/src/test/java/com/linkedin/venice/pubsub/adapter/kafka/producer/ApacheKafkaProducerConfigTest.java
@@ -1,5 +1,14 @@
 package com.linkedin.venice.pubsub.adapter.kafka.producer;
 
-public class ApacheKafkaProducerConfigTest {
+import com.linkedin.venice.exceptions.VeniceException;
+import com.linkedin.venice.utils.VeniceProperties;
+import org.testng.annotations.Test;
 
+
+public class ApacheKafkaProducerConfigTest {
+  @Test(expectedExceptions = VeniceException.class, expectedExceptionsMessageRegExp = ".* Required property: kafka.bootstrap.servers is missing.*")
+  public void testConfiguratorThrowsAnExceptionWhenBrokerAddressIsMissing() {
+    VeniceProperties veniceProperties = new VeniceProperties();
+    new ApacheKafkaProducerConfig(veniceProperties, null, true);
+  }
 }
