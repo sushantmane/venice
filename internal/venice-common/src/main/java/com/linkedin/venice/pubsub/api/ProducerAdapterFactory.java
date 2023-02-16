d0b01a305bc4b1ad27b0cd8be0ac71aae2a8f9ba
Sushant Mane <sumane@linkedin.com>
2023-02-15 23:35:05 -0800

    Add comments for ProducerAdapterFactory

--- internal/venice-common/src/main/java/com/linkedin/venice/pubsub/api/ProducerAdapterFactory.java
+++ internal/venice-common/src/main/java/com/linkedin/venice/pubsub/api/ProducerAdapterFactory.java
@@ -4,17 +4,20 @@ import com.linkedin.venice.utils.VeniceProperties;
 import java.io.Closeable;
 
 
+/**
+ * A producer factory interface that should be implemented to support writing venice data to a
+ */
 public interface ProducerAdapterFactory<ADAPTER extends ProducerAdapter> extends Closeable {
   /**
    *
-   * @param veniceProperties      A copy of venice properties. Relevant producer configs will be extracted
-   *                              from veniceProperties using prefix matching. For kafka related producer configs
-   *                              kafka, ApacheKafkaProducerConfig will extract all the configs that start with "kafka."
-   *                              prefix.
-   * @param producerName          Name of the producer. If not null used as client.id in case of kafka pubsub producer.
-   * @param targetBrokerAddress   Broker address to use when creating a producer. If this value is null, localBrokerAddress
-   *                              from the producer factory will be used.
-   * @return
+   * @param veniceProperties     A copy of venice properties. Relevant producer configs will be extracted from
+   *                             veniceProperties using prefix matching. For example, to construct kafka producer
+   *                             configs that start with "kafka." prefix will be used.
+   * @param producerName         Name of the producer. If not null, it will be used to set the context
+   *                             for producer thread.
+   * @param targetBrokerAddress  Broker address to use when creating a producer.
+   *                             If this value is null, local broker address present in veniceProperties will be used.
+   * @return                     Returns an instance of a producer adapter
    */
   ADAPTER create(VeniceProperties veniceProperties, String producerName, String targetBrokerAddress);
 }
