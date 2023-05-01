cfca8f398f6b7aacace1cddd597f9742dfea0989
Sushant Mane <sumane@linkedin.com>
2023-05-01 09:48:28 -0700

    Update PubSubConfigs

--- internal/venice-test-common/src/integrationtest/java/com/linkedin/venice/integration/utils/PubSubBrokerConfigs.java
+++ internal/venice-test-common/src/integrationtest/java/com/linkedin/venice/integration/utils/PubSubBrokerConfigs.java
@@ -6,10 +6,14 @@ import com.linkedin.venice.utils.TestMockTime;
 public class PubSubBrokerConfigs {
   private final ZkServerWrapper zkServerWrapper;
   private final TestMockTime mockTime;
+  private final String regionName;
+  private final String clusterName;
 
   private PubSubBrokerConfigs(Builder builder) {
     this.zkServerWrapper = builder.zkServerWrapper;
     this.mockTime = builder.mockTime;
+    this.regionName = builder.regionName;
+    this.clusterName = builder.clusterName;
   }
 
   public ZkServerWrapper getZkWrapper() {
@@ -20,9 +24,19 @@ public class PubSubBrokerConfigs {
     return mockTime;
   }
 
+  public String getRegionName() {
+    return regionName;
+  }
+
+  public String getClusterName() {
+    return clusterName;
+  }
+
   public static class Builder {
     private ZkServerWrapper zkServerWrapper;
     private TestMockTime mockTime;
+    private String regionName;
+    private String clusterName;
 
     public Builder setZkWrapper(ZkServerWrapper zkServerWrapper) {
       this.zkServerWrapper = zkServerWrapper;
@@ -31,6 +45,16 @@ public class PubSubBrokerConfigs {
 
     public Builder setMockTime(TestMockTime mockTime) {
       this.mockTime = mockTime;
+      return this;
+    }
+
+    public Builder setRegionName(String regionName) {
+      this.regionName = regionName;
+      return this;
+    }
+
+    public Builder setClusterName(String clusterName) {
+      this.clusterName = clusterName;
       return this;
     }
 
