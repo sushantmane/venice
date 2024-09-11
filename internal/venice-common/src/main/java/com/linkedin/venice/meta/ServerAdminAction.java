d706dbd74de66ef9313ae3c245f2d52b90b3aa8e
Sushant Mane <sushantmane7@gmail.com>
2024-09-11 02:48:31 -0700

    Add unit tests for ServerAdminAction & GrpcUtilsTest

--- internal/venice-common/src/main/java/com/linkedin/venice/meta/ServerAdminAction.java
+++ internal/venice-common/src/main/java/com/linkedin/venice/meta/ServerAdminAction.java
@@ -1,8 +1,19 @@
 package com.linkedin.venice.meta;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public enum ServerAdminAction {
   DUMP_INGESTION_STATE(0), DUMP_SERVER_CONFIGS(1);
 
+  private static final Map<Integer, ServerAdminAction> ADMIN_ACTION_MAP = new HashMap<>(2);
+
+  static {
+    for (ServerAdminAction action: values()) {
+      ADMIN_ACTION_MAP.put(action.getValue(), action);
+    }
+  }
   private final int value;
 
   ServerAdminAction(int value) {
@@ -13,13 +24,11 @@ public enum ServerAdminAction {
     return this.value;
   }
 
-  // get the enum value from the integer value
   public static ServerAdminAction fromValue(int value) {
-    for (ServerAdminAction action: ServerAdminAction.values()) {
-      if (action.getValue() == value) {
-        return action;
-      }
+    ServerAdminAction action = ADMIN_ACTION_MAP.get(value);
+    if (action == null) {
+      throw new IllegalArgumentException("Unknown server admin action value: " + value);
     }
-    throw new IllegalArgumentException("Invalid value for ServerAdminAction: " + value);
+    return action;
   }
 }
